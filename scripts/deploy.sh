b1548f06f3be46130e4f8cc98972742786a91051
sinuk <sinuk@telcoware.com>
2024-03-28 13:24:41 +0900

    codebuild test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ CURRENT_PID=$(ps -ef | grep java | grep demo* | awk '{print $2}')
 
 echo "Running PID: {$CURRENT_PID}"
 
-if "$CURRENT_PID" [ -z CURRENT_PID ] ; then
+if [ -z $CURRENT_PID ] ; then
     echo "Project is not running"
 <200b>else
     kill -9 $CURRENT_PID
@@ -13,6 +13,6 @@ fi
 
 echo "Deploy Project...."
 
-nohup java -jar demo-0.0.1-SNAPSHOT.jar >> demo.log &
+nohup ../java -jar demo-0.0.1-SNAPSHOT.jar >> demo.log &
 
 echo "Done"
