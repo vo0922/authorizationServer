e2e244a6e26d979d324882b222f4df6174117873
sinuk <sinuk@telcoware.com>
2024-03-28 13:47:52 +0900

    codebuild test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,6 +13,6 @@ fi
 
 echo "Deploy Project...."
 
-nohup java -jar /home/ubuntu/build/demo-0.0.1-SNAPSHOT.jar > /home/ubuntu/build/demo.log &
+nohup java -jar /home/ubuntu/build/demo-0.0.1-SNAPSHOT.jar > /home/ubuntu/build/demo.log 2>&1 &
 
 echo "Done"
